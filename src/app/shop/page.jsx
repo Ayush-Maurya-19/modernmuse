5d6d9f45bc7f9ff2e7a72bb09c69c4f84d9e080c
ayush-maurya-19 <ayushmaurya1901@gmail.com>
2024-06-21 00:12:25 +0530

    pages

---
+++ src/app/shop/page.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const Shop = () => {
+  return (
+    <main className='max-w-6xl '>
+      <div>
+        All Products
+      </div>
+
+    </main>
+  )
+}
+
+export default Shop
